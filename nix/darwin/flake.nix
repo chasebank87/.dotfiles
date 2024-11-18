a76b719b50f553082c8bd84951b9968938b04aba
Chase Elder <chase@Chases-M4-MacBook-Pro.local>
2024-11-17 19:12:44 -0500

    moved nix down one folder anded some new nix configs

--- nix/darwin/flake.nix
+++ nix/darwin/flake.nix
@@ -10,45 +10,81 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
   let
-    configuration = { pkgs, ... }: {
+    configuration = { pkgs, config, ... }: {
+
+			nixpkgs.config.allowUnfree = true;
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+      environment.systemPackages = [ 
+				pkgs.vim
+				pkgs.mkalias
+      ];
 
-	homebrew = {
-	  enable = true;
-      	  brews = [
-	    "git"
-	    "node"
-	    "ntfy"
-	    "pandoc"
-	    "starship"
-	  ];
-	  casks = [
-	    "anaconda"
-	    "arc"
-	    "cheatsheet"
-	    "cursor"
-	    "google-chrome"
-	    "hammerspoon"
-	    "karabiner-elements"
-	    "lm-studio"
-	    "loopback"
-	    "microsoft-teams"
-	    "obsidian"
-	    "pallotron-yubiswitch"
-	    "pika"
-	    "raycast"
-	    "setapp"
-	    "steam"
-	    "visual-studio-code"
-	    "vmware-horizon-client"
-	    "warp"
-	    "yubico-yubikey-manager"
-	  ];
-	};
+			fonts.packages = [
+				(pkgs.nerdfonts.override { fonts = [ "Hack" ]; })
+			];
+
+			system.activationScripts.applications.text = let
+				env = pkgs.buildEnv {
+					name = "system-applications";
+					paths = config.environment.systemPackages;
+					pathsToLink = "/Applications";
+				};
+			in
+				pkgs.lib.mkForce ''
+				# Set up applications.
+				echo "setting up /Applications..." >&2
+				rm -rf /Applications/Nix\ Apps
+				mkdir -p /Applications/Nix\ Apps
+				find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+				while read -r src; do
+					app_name=$(basename "$src")
+					echo "copying $src" >&2
+					${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+				done
+						'';
+			
+			system.defaults = {
+				dock.autohide = true;
+			};
+
+			homebrew = {
+				enable = true;
+				brews = [
+					"git"
+					"node"
+					"ntfy"
+					"pandoc"
+					"starship"
+				];
+				casks = [
+					"anaconda"
+					"arc"
+					"cheatsheet"
+					"cursor"
+					"google-chrome"
+					"hammerspoon"
+					"karabiner-elements"
+					"lm-studio"
+					"loopback"
+					"microsoft-teams"
+					"obsidian"
+					"pallotron-yubiswitch"
+					"pika"
+					"raycast"
+					"setapp"
+					"steam"
+					"visual-studio-code"
+					"vmware-horizon-client"
+					"warp"
+					"yubico-yubikey-manager"
+				];
+				taps = [
+				];
+				onActivation.cleanup = "zap";
+				onActivation.autoUpdate = true;
+				onActivation.upgrade = true;
+			};
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
