713351f9918873b9a9b414e46167731f85967e2f
Chase Elder <chase@Chases-M4-MacBook-Pro.local>
2024-11-19 01:47:26 -0500

    Added home-manager

---
+++ .config/nix/configuration.nix
@@ -0,0 +1,91 @@
+{ config, pkgs, lib, ... }: {
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = [ 
+    pkgs.vim
+    pkgs.mkalias
+  ];
+
+  fonts.packages = [
+    (pkgs.nerdfonts.override { fonts = [ "Hack" ]; })
+  ];
+
+  system.activationScripts.applications.text = let
+    env = pkgs.buildEnv {
+      name = "system-applications";
+      paths = config.environment.systemPackages;
+      pathsToLink = "/Applications";
+    };
+  in
+    pkgs.lib.mkForce ''
+      # Set up applications.
+      echo "setting up /Applications..." >&2
+      rm -rf /Applications/Nix\ Apps
+      mkdir -p /Applications/Nix\ Apps
+      find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+      while read -r src; do
+        app_name=$(basename "$src")
+        echo "copying $src" >&2
+        ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+      done
+    '';
+
+  system.defaults = {
+    dock.autohide = true;
+  };
+
+  system.defaults.finder = {
+    FXPreferredViewStyle = "clmv";
+    ShowPathbar = true;
+    ShowStatusBar = true;
+  };
+
+  homebrew = {
+    enable = true;
+    brews = [
+      "git"
+      "node"
+      "ntfy"
+      "pandoc"
+      "starship"
+      "neofetch"
+      "eza"
+      "zoxide"
+      "go"
+      "rust"
+      "lua"
+      "thefuck"
+      "bat"
+      "nushell"
+    ];
+    casks = [
+      "anaconda"
+      "arc"
+      "cheatsheet"
+      "cursor"
+      "google-chrome"
+      "hammerspoon"
+      "karabiner-elements"
+      "lm-studio"
+      "loopback"
+      "microsoft-teams"
+      "obsidian"
+      "pallotron-yubiswitch"
+      "pika"
+      "raycast"
+      "setapp"
+      "steam"
+      "visual-studio-code"
+      "vmware-horizon-client"
+      "warp"
+      "yubico-yubikey-manager"
+    ];
+    taps = [];
+    onActivation.cleanup = "zap";
+    onActivation.autoUpdate = true;
+    onActivation.upgrade = true;
+  };
+
+  nix.settings.experimental-features = "nix-command flakes";
+  system.stateVersion = 5;
+  nixpkgs.hostPlatform = "aarch64-darwin";
+}
---
+++ .zshrc
@@ -0,0 +1,41 @@
+export XDG_CONFIG_HOME="$HOME/.config"
+
+export PATH=/usr/local/anaconda3/bin:$PATH
+export PATH=/opt/homebrew/anaconda3/bin:$PATH
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/homebrew/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/homebrew/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/homebrew/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
+eval "$(starship init zsh)"
+
+# Added by LM Studio CLI (lms)
+export PATH="$PATH:/Users/chase/.cache/lm-studio/bin"
+
+# Bat theme
+export BAT_THEME="Dracula"
+
+# Zoxide (better cd)
+eval "$(zoxide init zsh)"
+ZVM_INIT_MODE=sourcing
+
+# Aliases
+alias ls='eza --icons=always'
+alias cd='z'
+alias cat='bat'
+eval $(thefuck --alias)
+eval $(thefuck --alias fk)
+
+neofetch
