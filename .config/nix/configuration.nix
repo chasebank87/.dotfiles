357e6d64c67161751daec6794b4fb9b148190299
chasebank87 <chase@chaseelder.com>
2025-03-02 15:48:45 -0500

    Nix Flake Update

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, self, ... }: {
   security.pam.services.sudo_local.touchIdAuth = true;
-  security.pam.services.sudo_local.watchIdAuth = true
+  security.pam.services.sudo_local.watchIdAuth = true;
 
   environment.systemPackages = [ 
     pkgs.vim
