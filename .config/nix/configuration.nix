3d343ab930ae27f8a9fcbefe1eb92be5af62cf7e
chasebank87 <chase@chaseelder.com>
2025-03-02 16:38:56 -0500

    Nix Flake Update

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -137,7 +137,6 @@
     onActivation.upgrade = true;
   };
 
-  nix.settings.experimental-features = "nix-command flakes";
   system.configurationRevision = self.rev or self.dirtyRev or null;
   system.stateVersion = 5;
   nixpkgs.hostPlatform = "aarch64-darwin";
